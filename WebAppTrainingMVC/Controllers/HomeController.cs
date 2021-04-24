ae635ecc199dd274914ac02a6efcd4a5d144a59e
ugonm <ugonmaforsblom@gmail.com>
2021-04-24 22:55:18 +0200

    my project,want it pushed to github

---
+++ WebAppTrainingMVC/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppTrainingMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        static List<string[]> contactList = new List<string[]> ();
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult About()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Contact()
+        {
+        return View();
+    }   [HttpPost]
+        public IActionResult Contact(string name,string phone,string email,string YourMessage)
+            
+        {
+            contactList.Add(new string[] { name, phone, email,YourMessage }); 
+            
+            ViewBag.contactList = contactList;
+
+
+            return View("contactList");
+            
+        }
+        public IActionResult Projects()
+        {
+            return View();
+        } 
+    }
+}
